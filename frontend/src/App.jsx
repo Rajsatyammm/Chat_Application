e45d2213aaa2ca12b18a644ff9f2927c713cd72a
Satyam Raj <rajsatyammm@gmail.com>
2025-02-02 15:00:31 +0530

    setting page set for accessing without auth.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/profile' element={<ProtectedRoute component={<ProfilePage />} />} />
-        <Route path='/settings' element={<ProtectedRoute component={<SettingsPage />} />} />
+        <Route path='/settings' element={<SettingsPage />} />
       </Routes>
       <Toaster />
     </div>
