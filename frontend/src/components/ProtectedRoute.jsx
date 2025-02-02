83f21210e088d5141689942349a74175d6a10365
Satyam Raj <rajsatyammm@gmail.com>
2025-02-02 12:58:26 +0530

    Auth check on every refresh implemented.

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect } from 'react';
 import Login from '../pages/Login';
 import { useAuthStore } from '../store/useAuthStore';
+import Loader from './Loader';
 
 const ProtectedRoute = ({ component }) => {
-    const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+    const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
     useEffect(() => {
         checkAuth();
     }, [checkAuth]);
 
+    if (isCheckingAuth) return <Loader />
     if (!authUser) return <Login />
 
     return component;
