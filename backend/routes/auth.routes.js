5abb0934f9ada08a0a15ed6cfe9f7b31b103d5cb
Satyam Raj <rajsatyammm@gmail.com>
2025-02-14 19:07:32 +0530

    multer dependency added.

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,14 +1,19 @@
 import express from 'express'
-import { checkAuth, getAllUsersForSidebar, login, logout, signUp, updateProfile } from '../controllers/auth.controller.js';
+import { checkAuth, getAllUsersForSidebar, getUserProfile, login, logout, signUp, updateProfile } from '../controllers/auth.controller.js';
 import { authMiddleware } from '../middlewares/auth.middleware.js';
+import multer from 'multer';
+
+const storage = multer.memoryStorage(); // Store file in memory temporarily
+const upload = multer({ storage: storage });
 
 const router = express.Router();
 
 router.post('/login', login)
 router.post('/signup', signUp)
 router.post('/logout', authMiddleware, logout)
-router.post('/update-profile', authMiddleware, updateProfile)
+router.put('/update-profile', upload.single('profilePic'), authMiddleware, updateProfile)
 
+router.get('/profile', authMiddleware, getUserProfile)
 router.get('/check', authMiddleware, checkAuth);
 router.get('/users', authMiddleware, getAllUsersForSidebar)
 
