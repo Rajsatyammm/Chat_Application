6037dde5edee413b05a05f5ac20199e594fe5b92
Satyam Raj <rajsatyammm@gmail.com>
2025-02-16 00:34:25 +0530

    cookies setting updated

--- backend/utils/jwtUtil.js
+++ backend/utils/jwtUtil.js
@@ -3,6 +3,13 @@ import jwt from 'jsonwebtoken'
 export const generateJwtToken = (user, res) => {
     const { _id, email, firstName, lastName, userName, createdAt, profilePic } = user;
     const token = jwt.sign({ _id: _id, email: email, firstName: firstName, lastName: lastName, userName: userName, createdAt, profilePic: profilePic }, process.env.JWT_SECRET, { expiresIn: '3d' });
-    res.cookie('token', token, { maxAge: 3 * 24 * 60 * 60 * 1000, httpOnly: true });
+    
+    res.cookie("token", token, {
+      maxAge: 3 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.MODE !== "development",
+    });
+
     return token;
 }
