cbbdc5c6d0286c2f9e24efbebdf3a83cca3245ca
Satyam Raj <rajsatyammm@gmail.com>
2025-02-16 09:59:26 +0530

    sameSite to none

--- backend/utils/jwtUtil.js
+++ backend/utils/jwtUtil.js
@@ -7,6 +7,7 @@ export const generateJwtToken = (user, res) => {
     res.cookie("token", token, {
       maxAge: 3 * 24 * 60 * 60 * 1000,
       httpOnly: true,
+      sameSite: "none",
       secure: process.env.MODE !== "development",
     });
 
