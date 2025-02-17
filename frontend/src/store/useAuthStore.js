f1f1b502c2167c6d6e182fe928eb98f06730d924
Satyam Raj <rajsatyammm@gmail.com>
2025-02-17 14:04:47 +0530

    minor issue fixed.

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -5,7 +5,9 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 import { getDecryptedObjectFromEncryptedString } from "../utils/util";
 
-const BASE_URL = import.meta.env.VITE_SERVER_BASE_URL;
+const BASE_URL = import.meta.env.MODE == "development"
+    ? import.meta.env.VITE_SERVER_BASE_URL_DEV
+    : import.meta.env.VITE_SERVER_BASE_URL_PROD;
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
